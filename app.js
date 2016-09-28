7c1737436310726a749dbbe6ceae4d7f56516938
Punit Singh <punit.singh@macys.com>
2016-09-28 11:10:51 -0700

    added nodejs also

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
 app.use("/",express.static(path.join(__dirname, 'dist')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 
 // catch 404 and forward to error handler
